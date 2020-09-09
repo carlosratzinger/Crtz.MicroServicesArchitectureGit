33073f36702a4ef05470cc2ef3a17d334a65fdd9
Carlos L <carlosratzinger@hotmail.com>
2020-09-09 11:10:45 -0300

    Endpoint "core" working

---
+++ Crtz.BasicContext.App.EPoint.Cmd/App_Start/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+
+namespace Crtz.BasicContext.App.EPoint.Cmd
+{
+    public static class Bootstrapper
+    {
+        public static IContainer Container { get; private set; }
+
+        public static void Initialize()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            //builder.RegisterType<ProductStorage>().As<IProductStorage>();
+            //--- Others
+
+            Container = builder.Build();
+        }
+    }
+}
--- Crtz.BasicContext.App.EPoint.Cmd/Program.cs
+++ Crtz.BasicContext.App.EPoint.Cmd/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using NServiceBus;
+using NServiceBus.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace Crtz.BasicContext.App.EPoint.Cmd
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const string endpointName = "BasicContext_EPoint";
+        private static ILog LOG = LogManager.GetLogger<Program>();
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = endpointName;
+
+            Bootstrapper.Initialize();
+            InitializeEndpoint().GetAwaiter().GetResult();
+        }
+
+        private static async Task InitializeEndpoint()
+        {
+            EndpointConfiguration endpointCfg = new EndpointConfiguration(endpointName);
+            endpointCfg.UseSerialization<NewtonsoftSerializer>();
+            endpointCfg.UseTransport<LearningTransport>();
+            endpointCfg.UsePersistence<LearningPersistence>();
+
+            IEndpointInstance endpointInstance = await Endpoint.Start(endpointCfg).ConfigureAwait(false);
+
+            Console.WriteLine($"{endpointName} endpoint started with success");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.WriteLine();
+
+            Console.ReadLine();
+            await endpointInstance.Stop().ConfigureAwait(false);
         }
     }
 }
